9f94f848974622496224327b66c3571891187b4d
kotka <47333381+ItzNinjaCat@users.noreply.github.com>
2023-03-25 19:42:03 +0200

    contract and subgraph redeploy

--- decentralised-ticketing-system-frontend/src/constants/constants.js
+++ decentralised-ticketing-system-frontend/src/constants/constants.js
@@ -1,6 +1,6 @@
-export const TIK_ADDRESS = '0x0554B393a6d9E96fb1686A3F4e8f36D36b0846a2';
-export const SOUVENIR_ADDRESS = '0x5c7e07F8FF22fe3F6D52AFbDD94f39013625D66F';
-export const TICKET_ADDRESS = '0x94A0824a11986777180A5D27ec12Ad829857ede2';
+export const TIK_ADDRESS = '0xd855e3D3fBAc38Db2559d8f919CaF7bB86720b03';
+export const SOUVENIR_ADDRESS = '0xE7c5cc0c51b67069c164d72426F7f82b3DB8E30b';
+export const TICKET_ADDRESS = '0x0F8e25d8DF69D7bb0311fEcb0EfF1Bf27ef72b33';
 export const SUBGRAPH_URL =
-    'https://api.studio.thegraph.com/query/39731/decentralised-ticketing-system/v0.3.5';
+  'https://api.studio.thegraph.com/query/39731/decentralised-ticketing-system/v0.3.6';
 export const W3LINK_URL = 'https://w3s.link/ipfs';
